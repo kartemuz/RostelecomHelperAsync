90c0c0b52b7f8357d4ebe778cbce622cae833456
veratium <veratiumlumus@gmail.com>
2022-07-22 07:48:23 +0300

    first commit

---
+++ relevance.py
@@ -0,0 +1,40 @@
+from config import dictionary
+
+
+#  {релевантность: заголовок}
+relevance_check = {}
+string = '''Извините, не можем ответить на ваш вопрос.
+Пожалуйста, обратитесь в службу поддержки по номеру: 8-800-1000-800'''
+
+
+#  Сравнивает введенные слова с добытыми с сайта
+#  question - введённые слова
+def entry_relevance(question):
+
+
+    try:
+        #  проверка на соответствие слов
+        for i in dictionary:
+            relevance_check[len(set(question.split()) & dictionary[i])] = i
+        relevance_check_value = relevance_check[max(relevance_check)]
+
+
+        if max(relevance_check) < 1:
+            relevance_check.clear()
+            return string
+        else:
+            relevance_check.clear()
+            return relevance_check_value
+
+
+    except Exception:
+        return string
+
+
+if __name__ == '__main__':
+    while True:
+        message = input('>>>')
+        if message == '0':
+            break
+        print(entry_relevance(message))
+        print()
